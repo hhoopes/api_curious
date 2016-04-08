d1a88d89ab67d76a8dc254a1b0539059f16316da
hhoopes <heidih@gmail.com>
2016-04-08 14:08:30 -0600

    Add user model test and view logged in info feature test

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -7,13 +7,13 @@ RSpec.feature "user logs in", vcr: true do
       @user = create(:user)
     end
 
-    it "allows user to log in" do
+    it "allows user to view info when logged in" do
       VCR.use_cassette("sessions#create") do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-      visit "/"
-      expect(current_path).to eq("/")
-      expect(page).to have_content(@user.first_name)
-      expect(page).to have_content("Neanderthal Profile")
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        visit "/"
+        expect(current_path).to eq("/")
+        expect(page).to have_content(@user.first_name)
+        expect(page).to have_content("Neanderthal Profile")
       end
     end
   end
